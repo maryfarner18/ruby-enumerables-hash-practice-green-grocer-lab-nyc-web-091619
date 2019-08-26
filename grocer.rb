3c53c483089564567c5c29030c5c3873ccac3ede
Mary Farner <maryfarner@gmail.com>
2019-08-26 17:43:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,12 +18,14 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
       thing = coupon[:item]
       new_name = "#{thing} W/COUPON"
-      
+    
       if cart.has_key?(thing) then
+        #Create the coupon item in the cart
         cart[new_name]= {}
         cart[new_name][:clearance] = true
         cart[new_name][:count] = cart[thing][:count] / coupon[:num] * 1 * cart[thing][:count]
 
+        #Adjust the old item in the cart w/o coupon
         cart[new_name][:price] = coupon[:cost] / coupon[:num]
         cart[thing][:count] %= coupon[:num]
         cart[thing][:clearance] = true
