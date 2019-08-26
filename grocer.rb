6c79758035944de2fd5fe1f7c7a6b313088ce623
Mary Farner <maryfarner@gmail.com>
2019-08-26 18:31:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,8 +44,9 @@ end
 
 
 def apply_clearance(cart)
-  cart.each_key() do |key|
-    cart[key][:price] *= (cart[key][:clearance]? .80 : 1)
+  cart.each_key do |key|
+    puts "hit"
+    #cart[key][:price] *= (cart[key][:clearance]? .80 : 1)
   end
   cart
 end
